278dfe90cb574e3afddbb52fe424eae6e3e18a67
Masta <mastaisshakh@gmail.com>
2026-01-30 23:00:15 +0300

    presque

--- amplify/functions/departments/list-departments/handler.ts
+++ amplify/functions/departments/list-departments/handler.ts
@@ -3,21 +3,39 @@ import {
   ListGroupsCommand,
 } from "@aws-sdk/client-cognito-identity-provider";
 
-const cognito = new CognitoIdentityProviderClient();
+import { isDeptGroup, keyToLabel } from "../_shared/departmentKey";
+
+const cognito = new CognitoIdentityProviderClient({});
+
+type Dept = { key: string; name: string };
 
 export const handler = async () => {
   const userPoolId = process.env.AMPLIFY_AUTH_USERPOOL_ID;
   if (!userPoolId) throw new Error("Missing AMPLIFY_AUTH_USERPOOL_ID");
 
-  const res = await cognito.send(new ListGroupsCommand({ UserPoolId: userPoolId, Limit: 60 }));
+  const departments: Dept[] = [];
+  let nextToken: string | undefined;
 
-  const departments = (res.Groups ?? [])
-    .map((g) => ({
-      key: g.GroupName ?? "",
-      name: g.Description || g.GroupName || "",
-    }))
-    .filter((d) => d.key.startsWith("DEPT_"))
-    .sort((a, b) => a.name.localeCompare(b.name));
+  do {
+    const res = await cognito.send(
+      new ListGroupsCommand({
+        UserPoolId: userPoolId,
+        NextToken: nextToken,
+        Limit: 60,
+      })
+    );
 
+    for (const g of res.Groups ?? []) {
+      const key = g.GroupName ?? "";
+      if (!isDeptGroup(key)) continue;
+
+      const name = (g.Description ?? "").trim() || keyToLabel(key);
+      departments.push({ key, name });
+    }
+
+    nextToken = res.NextToken;
+  } while (nextToken);
+
+  departments.sort((a, b) => a.name.localeCompare(b.name));
   return { departments };
 };
